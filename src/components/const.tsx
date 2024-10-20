485197f4a08496163d581655a6901b150435b0cc
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-10-20 21:58:45 +0800

    add more text

--- src/components/const.tsx
+++ src/components/const.tsx
@@ -6,6 +6,65 @@ export const TEXT = {
             merch: 'Merch',
             about: 'About',
             rule: 'Rules',
+        },
+        home: {
+            banner: {
+                bannerTitle1: 'Welcome to Xiaojean Gang ♡',
+                bannerTitle2: "I love playing games, drawing, and singing ♡",
+                bannerTitle3: "My simple wish is to",
+                bannerTitle4: 'bring joy to you during my stream time ♡',
+                buttonText: '《Go to watch Virtual skin Debut》',
+            },
+            section1: {
+                intro: {
+                    title: 'Xiaojean',
+                    age: 'Age: 20',
+                    birthday: 'Birthday: 1/28',
+                    favoriteFood: 'Favorite Food: Curry',
+                    hobby: 'Hobbies: Singing, drawing, gaming',
+                    favoriteGame: 'Games: Mainly Valorant, occasionally trying other games',
+                    notice1: 'Currently working a full-time job, not a full-time VTuber',
+                    notice2: 'Please don’t fall for me; I only show favoritism towards girls',
+                    notice3: 'Collab invites 💌: xiaojean0128@gmail.com',
+                },
+                linkItem: {
+                    marshmallow: {
+                        title: 'Marshmallow',
+                        subtitle: 'Say something you don’t dare to say during the stream here.',
+                    },
+                    twitch: {
+                        title: 'Twitch',
+                        subtitle: 'Regular streams and stream event updates.',
+                    },
+                    instagram: {
+                        title: 'Instagram',
+                        subtitle: 'Event posts, photos, and REELS.',
+                    },
+                    youtube: {
+                        title: 'YouTube',
+                        subtitle: 'Highlights, covers, and shorts.',
+                    },
+                    facebook: {
+                        title: 'Facebook',
+                        subtitle: 'Event posts and commissioned artworks.',
+                    },
+                    twitter: {
+                        title: 'Twitter',
+                        subtitle: 'Event announcements and appreciation posts.',
+                    },
+                    discord: {
+                        title: 'Discord',
+                        subtitle: 'You might catch me in member voice channels occasionally.',
+                    }
+                }
+            },
+            rankVideoTitle: 'Valorant Extraordinary Journey',
+            episodeTitle: 'Select episode',
+            episode1: 'Ep.1 - The opponents never give an ACE😭',
+            episode2: 'Ep.2 - Reyna 1v4 Clutch Situation in rank',
+            episode3: 'Ep.3 - Losing streak in Platinum ranked games😭',
+            episode4: 'Ep.4 - Encountered a cheater in ranked again?!',
+            episode5: 'Ep.5 - Facing multiple opponents in clutch situation with Reyna again!',
         }
     },
     zh: {
@@ -15,6 +74,65 @@ export const TEXT = {
             merch: '周邊',
             about: '關於我',
             rule: '互動規則',
+        },
+        home: {
+            banner: {
+                bannerTitle1: "歡迎加入心結幫 ♡",
+                bannerTitle2: "我喜歡玩遊戲、畫畫、唱歌 ♡",
+                bannerTitle3: "簡單的心願就是",
+                bannerTitle4: '希望能在直播的時間讓你們感到快樂 ♡',
+                buttonText: '《前往觀看V皮初配信》',
+            },
+            section1: {
+                intro: {
+                    title: '小靜しずか',
+                    age: '年齡：20',
+                    birthday: '生日：1/28',
+                    favoriteFood: '食物：咖哩',
+                    hobby: '興趣：唱歌畫畫遊戲',
+                    favoriteGame: '遊戲：特戰為主，偶爾嘗試其他遊戲',
+                    notice1: '目前有正職，為非全職套皮實況主',
+                    notice2: '請勿暈船，我只對妹子有差別待遇',
+                    notice3: '聯動邀約💌：xiaojean0128@gmail.com',
+                },
+                linkItem: {
+                    marshmallow: {
+                        title: '棉花糖',
+                        subtitle: '不敢在直播上跟我說的話可以丟這',
+                    },
+                    twitch: {
+                        title: 'Twitch',
+                        subtitle: '常駐直播、直播活動限動',
+                    },
+                    instagram: {
+                        title: 'Instagram',
+                        subtitle: '活動貼文、照片限動、REELS',
+                    },
+                    youtube: {
+                        title: 'Youtube',
+                        subtitle: '精華影片、翻唱、shorts',
+                    },
+                    facebook: {
+                        title: 'Facebook',
+                        subtitle: '活動貼文、委託繪畫作品',
+                    },
+                    twitter: {
+                        title: 'Twitter',
+                        subtitle: '活動貼文、感謝貼文',
+                    },
+                    discord: {
+                        title: 'Discord',
+                        subtitle: '平時有機會出現在會員語音',
+                    }
+                }
+            },
+            rankVideoTitle: '特戰超凡之旅',
+            episodeTitle: '選擇集數',
+            episode1: '第1集 - 對手永遠不給ACE😭',
+            episode2: '第2集 - 排位蕾娜1打4殘局！',
+            episode3: '第3集 - 白金場排位連輸心態爆炸😭',
+            episode4: '第4集 - 排位又遇外掛?!照樣1發拿下MVP😎',
+            episode5: '第5集 - 蕾娜再次一打多殘局',
         }
     },
 };
