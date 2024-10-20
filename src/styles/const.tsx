83e3ff692a1c1ac430c8fdee967fde9259af73b6
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-10-20 21:59:42 +0800

    add brackground image

--- src/styles/const.tsx
+++ src/styles/const.tsx
@@ -1,7 +1,15 @@
+export const GLOBAL = {
+    backgroundColor: 'rgba(206, 163, 179, 0.6)',
+}
+
 export const HEADER = {
     height: 70, // header高度
     logoFontSize: 20, // LOGO文字大小
     navTextColor: '#D4EDFF',
     navTextFontSize: 18,
     backgroundColor: '#242734', // header背景色
+}
+
+export const FOOTER = {
+    backgroundColor: '#242734'
 }
