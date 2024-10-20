83c9028cfa76b23943d79bbcabf82955dee6c6e9
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-10-20 21:59:19 +0800

    add component folder in style

--- src/components/switch_language_button.tsx
+++ src/components/switch_language_button.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import '@/styles/switch_language_button.css';  // 導入CSS
+import React from 'react';
+import '@/styles/components/switch_language_button.css';  // 導入CSS
 import { SwitchLanguageButtonProps } from '@/components/interface';
 
 export const SwtichLanguageButton: React.FC<SwitchLanguageButtonProps> = ({ language, onClick }) => {
