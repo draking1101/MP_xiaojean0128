b90170cae47e5e33c4f905e989dbcdbf9389e181
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-10-23 16:49:44 +0800

    refactor styles

--- src/pages/merch.tsx
+++ src/pages/merch.tsx
@@ -1,109 +1,94 @@
-import React from 'react';
 // Styles
-import { GLOBAL, HEADER } from '@/styles/const';
 import '@/styles/merch.css';
 
 export const Merch = () => {
     return (
-        <div style={styles.merch} id="merch-body" className="page-body">
-            <div id="merch-area">
-                <div className="merch-outterdiv">
+        <div className="page-body">
+            <div className="merch-body">
+                <div className="merch-area">
                     {/* 限時周邊 */}
-                    <h1>限時周邊</h1>
-                    <ul>
-                        <li>
+                    <h1 className='merch-area-title'>限時周邊</h1>
+                    <ul className='merch-item-container'>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/A.無分類/003-小靜.png')} alt="Lost" />
-                            <b><p className="merch-title">敬請期待</p></b>
-                            <p className="merch-text"></p>
-                            <a target="_blank" rel="noreferrer"><p>《敬請期待》</p></a>
+                            <p className="merch-item-title">敬請期待</p>
+                            <p className="merch-item-text"></p>
+                            <a className='merch-item-link' target="_blank" rel="noreferrer"><p>《敬請期待》</p></a>
                         </li>
-                        {/* <li>
+                        {/* <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/A.無分類/003-小靜.png')} alt="Lost" />
-                            <b><p className="merch-title">敬請期待</p></b>
-                            <p className="merch-text"></p>
-                            <a href="/#" target="_blank"  rel="noreferrer"><p>《敬請期待》</p></a>
+                            <p className="merch-item-title">敬請期待</p>
+                            <p className="merch-item-text"></p>
+                            <a className='merch-item-link' href="/#" target="_blank"  rel="noreferrer"><p>《敬請期待》</p></a>
                         </li> */}
                     </ul>
                 </div>
                 {/* 常駐周邊 */}
-                <div className="merch-outterdiv">
-                    <h1>常駐周邊</h1>
-                    <ul>
-                        {/* <li>
-                            <img src={require('@/assets/xiaojean/A.無分類/003-小靜.png')} alt="Lost" />
-                            <b><p className="merch-title">敬請期待</p></b>
-                            <p className="merch-text"></p>
-                            <a href="/#" target="_blank"  rel="noreferrer"><p>《敬請期待》</p></a>
-                        </li> */}
-                        <li>
+                <div className="merch-area">
+                    <h1 className='merch-area-title'>常駐周邊</h1>
+                    <ul className='merch-item-container'>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/007-Lline貼圖(去背).png')} alt="Lost" />
-                            <b><p className="merch-title">小靜Line貼圖</p></b>
-                            <p className="merch-text"></p>
-                            <a href="https://store.line.me/stickershop/product/25838548/zh-Hant?utm_source=gnsh_stickerDetail" target="_blank" rel="noreferrer">
+                            <p className="merch-item-title">小靜Line貼圖</p>
+                            <p className="merch-item-text"></p>
+                            <a className='merch-item-link' href="https://store.line.me/stickershop/product/25838548/zh-Hant?utm_source=gnsh_stickerDetail" target="_blank" rel="noreferrer">
                                 《點擊前往周邊》
                             </a>
                         </li>
-                        <li>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/008-Lline表情符號(去背).png')} alt="Lost" />
-                            <b><p className="merch-title">小靜Line表情符號</p></b>
-                            <p className="merch-text"></p>
-                            <a href="https://store.line.me/emojishop/product/65d99d86357e3d15ccfcf4be/zh-Hant?lang=zh-Hant&utm_source=gnsh_sticonDetail" target="_blank" rel="noreferrer">
+                            <p className="merch-item-title">小靜Line表情符號</p>
+                            <p className="merch-item-text"></p>
+                            <a className='merch-item-link' href="https://store.line.me/emojishop/product/65d99d86357e3d15ccfcf4be/zh-Hant?lang=zh-Hant&utm_source=gnsh_sticonDetail" target="_blank" rel="noreferrer">
                                 《點擊前往周邊》
                             </a>
                         </li>
-                        {/* <li>
+                        {/* <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/A.無分類/003-小靜.png')} alt="Lost" />
-                            <b><p className="merch-title">敬請期待</p></b>
-                            <p className="merch-text"></p>
-                            <a href="/#" target="_blank"  rel="noreferrer"><p>《敬請期待》</p></a>
-                        </li> */}
-                        {/* <li>
-                            <img src={require('@/assets/xiaojean/A.無分類/003-小靜.png')} alt="Lost" />
-                            <b><p className="merch-title">敬請期待</p></b>
-                            <p className="merch-text"></p>
-                            <a href="/#" target="_blank"  rel="noreferrer"><p>《敬請期待》</p></a>
+                            <p className="merch-item-title">敬請期待</p>
+                            <p className="merch-item-text"></p>
+                            <a className='merch-item-link' href="/#" target="_blank"  rel="noreferrer"><p>《敬請期待》</p></a>
                         </li> */}
                     </ul>
                 </div>
                 {/* 絕版周邊 */}
-                <div className="merch-outterdiv">
-                    <h1>絕版周邊</h1>
-                    <ul>
-                        <li>
+                <div className="merch-area">
+                    <h1 className='merch-area-title'>絕版周邊</h1>
+                    <ul className='merch-item-container'>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/001-一周年周邊套組.png')} alt="Lost" />
-                            <b><p className="merch-title">一周年周邊套組</p></b>
-                            <p className="merch-text">含立牌、飯友、吊飾</p>
+                            <p className="merch-item-title">一周年周邊套組</p>
+                            <p className="merch-item-text">含立牌、飯友、吊飾</p>
                         </li>
-                        <li>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/002-千訂回饋周邊.png')} alt="Lost" />
-                            <b><p className="merch-title">千訂回饋周邊</p></b>
-                            <p className="merch-text">含滑鼠墊 & 雙面抱枕</p>
+                            <p className="merch-item-title">千訂回饋周邊</p>
+                            <p className="merch-item-text">含滑鼠墊 & 雙面抱枕</p>
                         </li>
-                        <li>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/003-二周年募資限定周邊_吊飾and飯友.jpg')} alt="Lost" />
-                            <b><p className="merch-title">二周年募資限定周邊</p></b>
-                            <p className="merch-text">吊飾 & 飯友</p>
+                            <p className="merch-item-title">二周年募資限定周邊</p>
+                            <p className="merch-item-text">吊飾 & 飯友</p>
                         </li>
-                        <li>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/004-二周年募資限定周邊_特殊悠遊卡and卡套.jpg')} alt="Lost" />
-                            <b><p className="merch-title">二周年募資限定周邊</p></b>
-                            <p className="merch-text">特殊悠遊卡 & 卡套</p>
+                            <p className="merch-item-title">二周年募資限定周邊</p>
+                            <p className="merch-item-text">特殊悠遊卡 & 卡套</p>
                         </li>
-                        <li>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/005-二周年回饋周邊_Q版立牌(多邊框+Logo).png')} alt="Lost" />
-                            <b><p className="merch-title">二周年回饋周邊</p></b>
-                            <p className="merch-text">Q版立牌</p>
+                            <p className="merch-item-title">二周年回饋周邊</p>
+                            <p className="merch-item-text">Q版立牌</p>
                         </li>
-                        <li>
+                        <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/006-FF43周邊.jpg')} alt="Lost" />
-                            <b><p className="merch-title">FF43 周邊</p></b>
-                            <p className="merch-text">簽名掛軸、明信片、帆布袋</p>
+                            <p className="merch-item-title">FF43 周邊</p>
+                            <p className="merch-item-text">簽名掛軸、明信片、帆布袋</p>
                         </li>
-                        {/* <li>
+                        {/* <li className='merch-item'>
                             <img src={require('@/assets/xiaojean/B.周邊/003-小靜.png')} alt="Lost" />
-                            <b><p className="merch-title">敬請期待</p></b>
-                            <p className="merch-text"></p>
-                            <a href="/#" target="_blank"  rel="noreferrer"><p>《敬請期待》</p></a>
+                            <p className="merch-item-title">敬請期待</p>
+                            <p className="merch-item-text"></p>
                         </li> */}
                     </ul>
                 </div>
@@ -111,13 +96,4 @@ export const Merch = () => {
         </div >
 
     )
-}
-
-const styles: { [key: string]: React.CSSProperties } = {
-    merch: {
-        marginTop: HEADER.height,
-        height: 'auto',
-        backgroundColor: GLOBAL.backgroundColor,
-
-    }
 }
--- src/pages/rule.tsx
+++ src/pages/rule.tsx
@@ -1,19 +1,64 @@
 import React from 'react';
-import { HEADER } from '@/styles/const';
+// Styles
+import '@/styles/rule.css';
 
 export const Rule = () => {
     return (
-        <div style={styles.rule}>
-            <h1>
-                Hello, rule!
-            </h1>
+        <div id="rule-body" className="page-body">
+            <div className="rule-div">
+                <div className="rule-textdiv">
+                    <h1>直播聊天室守則</h1>
+                    <p>
+                        <ul>
+                            <ol>
+                                <li>聊天室保持開心尊重的氛圍~</li>
+                                <li>請勿談及18禁及政治言論</li>
+                                <li>請勿傳送廣告與垃圾訊息</li>
+                                <li>請勿人身攻擊、侮辱謾罵任何人</li>
+                                <li>請勿一直重複兌換忠誠點數洗頻</li>
+                                <li>請勿情勒or洗版及詢問太過私人的問題</li>
+                                <li>開玩笑以及使用地獄梗請適度</li>
+                                <li>以上未提到但讓我很不舒服就是NG的</li>
+                            </ol>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+            <div className="rule-div">
+                <div className="rule-textdiv">
+                    <h1>Discord社群互動守則</h1>
+                    <p>
+                        <ul>
+                            <ol>
+                                <li>避免提及政治話題及歧視、仇恨等言論。</li>
+                                <li>禁止吵架及人身攻擊，有糾紛盡量私下解決。</li>
+                                <li>禁止騷擾任何人，也避免@所有人，不接受友邀，但有重要的事還是可以私訊我。</li>
+                                <li>如發現有違反規則的行為可以至<span style={{ color: '#ffd3e4' }}> ⁠⛔違規檢舉📬 </span>檢舉，經查證提醒後仍不改進會警告一次。</li>
+                                <li>根據嚴重性不同有可能直接踢出，最多警告三次一定會封鎖踢出。</li>
+                                <li>以上沒提到但有會影響他人的行為也會視情況封鎖踢出，本群我是規則，請不要觸犯我的底線。</li>
+                                <li>請記得至 ⁠<span style={{ color: '#ffd3e4' }}>🆔心結登記區📋 </span>登記身分組，若未登記會被定時踢出群組。</li>
+                                <li>請記得至 ⁠<span style={{ color: '#ffd3e4' }}>⁠♒會員申請📋 </span>申請會員身分組(記得看釘選的格式喔!)。</li>
+                                <li>每個頻道有他該做的事，記得看一下頻道說明，違反的我可能會刪除該訊息。</li>
+                                <li>群組永久連結：<a href='https://discord.gg/xiaojeandc' target='_black' rel='noreferrer'>https://discord.gg/xiaojeandc</a></li>
+                            </ol>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+            <div className="rule-div">
+                <div className="rule-textdiv">
+                    <h1>粉絲繪圖守則</h1>
+                    <p>
+                        <ul>
+                            <ol>
+                                <li>禁止直接投餵AI生成圖，單純文字詠唱可✅</li>
+                                <li>禁止R18創作，R16擦邊可✅</li>
+                                <li>若真的很想創作R18的話請不要上tag讓我看到，私底下自己創作我不會在意⁠<span style={{ color: '#ffd3e4' }}>（重點是不要讓我看到）</span></li>
+                            </ol>
+                        </ul>
+                    </p>
+                </div>
+            </div>
         </div>
     )
-}
-
-const styles: { [key: string]: React.CSSProperties } = {
-    rule: {
-        marginTop: HEADER.height,
-        height: 1000,
-    }
 }
