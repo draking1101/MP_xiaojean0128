007f3c64dabde34cad34c7ff618a307a079a2c6a
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-11-04 16:33:42 +0800

    加入 漢堡選單

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+// Images
+import logo from "@/assets/xiaojean/A.無分類/003-小靜.png"  // Logo
 // Styles
 import { HEADER } from '@/styles/const';
 import '@/styles/header.css';  // 導入CSS
@@ -10,33 +12,77 @@ import { useLanguage } from '@/components/LanguageContext';
 
 export const Header = () => {
     const { language, changeLanguage } = useLanguage();
+    const [openMenu, setOpenMenu] = useState(false); // 漢堡選單狀態
+
+    useEffect(() => {
+        // 當視窗寬度低於720px時，如果關閉漢堡選單在將視窗寬度恢復到 > 720px 會發生 導覽列消失的狀況
+        // 因此添加監聽器監控寬度變化，當 > 720px 恢復顯示導覽列
+        const handleResize = () => {
+            const navList = document.getElementById("nav-list");
+            if (window.innerWidth >= 720) {
+                navList.style.display = "flex";
+            } else {
+                navList.style.display = "none";
+            }
+        };
+
+        handleResize();
+
+        // 添加視窗大小監聽器
+        window.addEventListener("resize", handleResize);
+
+        // 在組件卸載後，清除監聽器
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    // 開關漢堡選單
+    function toggleMenu() {
+        const headerbody = document.getElementById("header-body")
+        const nav = document.getElementById("nav")
+        const navList = document.getElementById("nav-list")
+
+        if (!openMenu) {
+            nav.style.display = "flex"
+            headerbody.style.height = "550px"
+            navList.style.display = "flex"
+        } else {
+            headerbody.style.height = "70px"
+            navList.style.display = "none"
+            window.scrollTo({ top: 0, behavior: 'smooth' }); // 回到頁首
+        }
+        setOpenMenu(!openMenu);
+    }
 
     return (
-        <header style={styles.header}>
+        <header id='header-body'>
+            {/* 漢堡選單 */}
+            <div id="burger-menu-button" onClick={toggleMenu}>
+                <div>{(openMenu === true) ? "✖︎" : "☰"}</div>
+            </div>
             {/* LOGO 部分 */}
-            <div style={styles.logoContainer} className='logoContainer'>
+            <div className='logoContainer'>
                 <Link to="/" style={styles.logoLink}>
-                    <img src={require('@/assets/xiaojean/A.無分類/003-小靜.png')} alt="Logo" style={styles.logo} />
+                    <img src={logo} alt="Logo" style={styles.logo} />
                     <span style={styles.logoText}>小靜しずか</span>
                 </Link>
             </div>
 
             {/* 導航欄 */}
-            <nav style={styles.nav} className='nav'>
-                <ul style={styles.navList}>
-                    <li>
+            <nav id='nav' className='nav'>
+                <ul id='nav-list'>
+                    <li onClick={toggleMenu}>
                         <Link to="/" style={styles.navLink}>{TEXT[language].header.home}</Link>
                     </li>
-                    <li>
+                    <li onClick={toggleMenu}>
                         <Link to="/news" style={styles.navLink}>{TEXT[language].header.news}</Link>
                     </li>
-                    <li>
+                    <li onClick={toggleMenu}>
                         <Link to="/merch" style={styles.navLink}>{TEXT[language].header.merch}</Link>
                     </li>
-                    <li>
+                    <li onClick={toggleMenu}>
                         <Link to="/about" style={styles.navLink}>{TEXT[language].header.about}</Link>
                     </li>
-                    <li>
+                    <li onClick={toggleMenu}>
                         <Link to="/rule" style={styles.navLink}>{TEXT[language].header.rule}</Link>
                     </li>
                 </ul>
@@ -53,25 +99,6 @@ export const Header = () => {
 
 // 樣式
 const styles: { [key: string]: React.CSSProperties } = {
-    header: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        height: HEADER.height,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: HEADER.backgroundColor,
-        color: '#fff',
-        width: '100%',
-        zIndex: 100,
-    },
-    logoContainer: {
-        flex: '1',
-        display: 'flex',
-        alignItems: 'center',
-        paddingLeft: '20px',
-    },
     logoLink: {
         display: 'flex',
         alignItems: 'center',
@@ -85,11 +112,6 @@ const styles: { [key: string]: React.CSSProperties } = {
         fontSize: HEADER.logoFontSize,
         fontWeight: 'bold',
         color: '#ffd3e4',
-    },
-    nav: {
-        flex: '2',
-        textAlign: 'right',
-        paddingRight: '20px',
     },
     navList: {
         listStyle: 'none',
@@ -105,5 +127,3 @@ const styles: { [key: string]: React.CSSProperties } = {
         fontWeight: '500',
     }
 };
-
-// export default Header;
