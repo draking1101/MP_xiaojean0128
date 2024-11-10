4d8db04efd7d7401f4ecdcd8d74418b3ec64b9c8
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-11-10 22:18:31 +0800

    Ver.1.1.7
    1. 修正消息1 2的超連結相反的問題

--- src/pages/news_details/news_0-25.tsx
+++ src/pages/news_details/news_0-25.tsx
@@ -10,7 +10,7 @@ export const News0 = ({ selectedNews }) => {
                 <img style={{ width: 28, height: 28 }} src="https://cdn.discordapp.com/emojis/1211065179161231461.webp?size=44&quality=lossless" alt="Missing Image" />
             </P><br />
             <div style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
-                <a style={{ fontSize: '18px' }} href="https://youtube.com/playlist?list=PLiJk1NbL7YBLjA7u9iUFOTmML9PMJUljS&si=SjKCyBGMkLgoGLhC">
+                <a style={{ fontSize: '18px' }} href="https://youtube.com/playlist?list=PLc-iMbctKKIzEGiaV2WCHxpnjkSMKyKaI&si=zPxCs2L7fj54dWgb" target="_blank" rel="noreferrer">
                     {selectedNews.linkText}
                 </a>
             </div>
@@ -27,7 +27,7 @@ export const News1 = ({ selectedNews }) => {
             <P>{selectedNews.content4}</P><br />
             <P>{selectedNews.content5}</P><br />
             <div style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
-                <a style={{ fontSize: '18px' }} href="https://youtube.com/playlist?list=PLc-iMbctKKIzEGiaV2WCHxpnjkSMKyKaI&si=zPxCs2L7fj54dWgb">
+                <a style={{ fontSize: '18px' }} href="https://youtube.com/playlist?list=PLiJk1NbL7YBLjA7u9iUFOTmML9PMJUljS&si=SjKCyBGMkLgoGLhC" target="_blank" rel="noreferrer">
                     {selectedNews.linkText}
                 </a>
             </div>
